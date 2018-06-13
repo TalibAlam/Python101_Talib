59147cf933fe49e5c5a33e6975b32c410fed9a3b
TalibAlam <talibalam28"gmail.com>
2018-06-14 01:58:07 +0530

    Day3 Assignment

---
+++ day3/1.py
@@ -0,0 +1,9 @@
+for i in range(1,100):
+	if int(i)%3==0 and int(i)%5==0:
+		print('fizzbuzz')
+	elif int(i)%3==0:
+		print('fizz')
+	elif int(i)%5==0:
+		print('buzz')
+	else:
+		print(i)
---
+++ day3/2b.py
@@ -0,0 +1,19 @@
+n=int(input('Enter odd value for n: \t'))
+s=n
+p=3
+
+for i in range(n):
+    if i>int(n/2):
+        for k in range(int(i+1),n):
+               print(' ',end='')
+        for j in range(int(p)):
+               print('*',end='')
+        print('\n')
+        p=p+2
+    else:
+     for k in range(i):
+          print(' ',end='')
+     for j in range(s):
+          print('*',end='')
+     print('\n')
+     s=s-2
